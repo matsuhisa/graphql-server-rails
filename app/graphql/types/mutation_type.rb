649e6cb77fe0e80f517418315f729b79e9188986
matsuhisa <matsuhisa+github@gmail.com>
2021-05-08 23:15:13 +0900

    rails g graphql:mutation CreatePhoto

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :create_photo, mutation: Mutations::CreatePhoto
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
