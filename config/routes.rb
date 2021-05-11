a46d42ed0225665f94ff2c3861e736da198b090a
matsuhisa <matsuhisa+github@gmail.com>
2021-05-12 00:24:55 +0900

    Add root_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+  root to: "top#index"
   post "/graphql", to: "graphql#execute"
   resources :photos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
