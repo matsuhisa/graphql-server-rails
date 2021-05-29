016b92b8d2926ccf785fc14dea196300d5e48ba5
matsuhisa <matsuhisa+github@gmail.com>
2021-05-29 23:58:05 +0900

    Add relation photo <-> photo_tag <-> tag

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,8 @@ class Photo < ApplicationRecord
   validates :title, length: { in: 2..5 }
   validates :image_url, presence: true, format: /\A#{URI::regexp(%w(http https))}\z/
 
+  has_many :photo_tags
+  has_many :tags, through: :photo_tags
+
   enum category: { selfie: 0, landscape: 1, graphic: 2 }
 end
