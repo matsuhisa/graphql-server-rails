67ad0f87e7db14d7d530ae47808342f302eb0472
matsuhisa <matsuhisa+github@gmail.com>
2021-05-29 23:58:47 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_143443) do
+ActiveRecord::Schema.define(version: 2021_05_29_144146) do
 
   create_table "google_credentials", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2021_05_29_143443) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_google_credentials_on_user_id", unique: true
+  end
+
+  create_table "photo_tags", force: :cascade do |t|
+    t.integer "photo_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["photo_id"], name: "index_photo_tags_on_photo_id"
+    t.index ["tag_id"], name: "index_photo_tags_on_tag_id"
   end
 
   create_table "photos", force: :cascade do |t|
@@ -43,4 +52,6 @@ ActiveRecord::Schema.define(version: 2021_05_29_143443) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "photo_tags", "photos"
+  add_foreign_key "photo_tags", "tags"
 end
